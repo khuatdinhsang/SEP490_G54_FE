e7b1f5b04244805944d6f4eeb3d5723a5cd8f3ca
khuatdinhsang <sangkdhe161711@fpt.edu.vn>
2024-06-04 13:19:13 +0700

    done page register step 1 2 3 and login

--- src/page/login/index.tsx
+++ src/page/login/index.tsx
@@ -1,10 +1,151 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState } from 'react';
+import * as yup from "yup";
+import { Button, Image, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { ParamListBase, useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { SCREENS_NAME } from '../../navigator/const';
+import { useTranslation } from 'react-i18next';
+import colors from '../../constant/color';
+import { Formik } from 'formik';
+import { ScrollView } from 'react-native-gesture-handler';
+import axios from 'axios';
+import InputComponent from '../../component/input';
+import { flexRowCenter } from '../../styles/flex';
 
-const Login = () => {
-    return (
-        <Text>index</Text>
-    )
+interface LoginValues {
+    email: string;
+    password: string;
 }
 
-export default Login
+const Login = () => {
+    const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+    const { t } = useTranslation();
+    const [account, setAccount] = useState<any>('')
+
+    const loginSchema = yup.object().shape({
+        email: yup
+            .string()
+            .email(t("placeholder.err.email"))
+            .required(t("placeholder.err.blank")),
+        password: yup
+            .string()
+            .required(t("placeholder.err.blank"))
+    });
+
+    const handleRegister = () => {
+        navigation.navigate(SCREENS_NAME.REGISTER.STEP1)
+    }
+
+    const handleSubmit = async (values: LoginValues): Promise<any> => {
+        console.log("41", values)
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        console.log("43", response.data)
+    }
+
+    const clearEmail = (setFieldValue: (field: string, value: any) => void) => {
+        setFieldValue('email', '');
+    };
+
+    const clearPassword = (setFieldValue: (field: string, value: any) => void) => {
+        setFieldValue('password', '');
+    };
+
+    const handleFindId = () => {
+        // Handle find ID logic
+    }
+
+    const handleFindPassword = () => {
+        navigation.navigate(SCREENS_NAME.FORGOT_PASSWORD.VERIFY_EMAIL)
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView>
+                <Text style={styles.firstStep}>{t("common.text.firstStep")}</Text>
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validationSchema={loginSchema}
+                    onSubmit={handleSubmit}
+                >
+                    {({ handleChange, handleBlur, handleSubmit, setFieldValue, values, errors, touched }) => {
+                        console.log("ew", touched)
+                        return (
+                            <View>
+                                <View style={{ marginTop: 40 }}>
+                                    <View>
+                                        <InputComponent
+                                            placeholder={t("placeholder.field.email")}
+                                            onPressIconRight={() => clearEmail(setFieldValue)}
+                                            isIconRight={true}
+                                            value={values.email}
+                                            onChangeText={handleChange('email')}
+                                            label={t("authentication.email")}
+                                            textError={errors.email}
+                                        />
+                                    </View>
+                                    <View style={{ marginTop: 20 }}>
+                                        <InputComponent
+                                            placeholder={t("placeholder.field.password")}
+                                            onPressIconRight={() => clearPassword(setFieldValue)}
+                                            isIconRight={true}
+                                            value={values.password}
+                                            onChangeText={handleChange('password')}
+                                            label={t("authentication.password")}
+                                            secureTextEntry={true}
+                                            textError={errors.password}
+                                        />
+                                    </View>
+                                </View>
+                                <View style={{ marginTop: 30 }}>
+                                    <Pressable onPress={() => handleSubmit()} style={[styles.button, { backgroundColor: colors.primary }]}>
+                                        <Text style={styles.text}>{t("authentication.login")}</Text>
+                                    </Pressable>
+                                    <Pressable onPress={handleRegister} style={[styles.button, { backgroundColor: colors.black }]}>
+                                        <Text style={styles.text}>{t("authentication.register")}</Text>
+                                    </Pressable>
+                                    <View style={flexRowCenter}>
+                                        <Pressable onPress={handleFindId}>
+                                            <Text> {t("authentication.findId")}  |  </Text>
+                                        </Pressable>
+                                        <Pressable onPress={handleFindPassword}>
+                                            <Text>{t("authentication.findPassword")}</Text>
+                                        </Pressable>
+                                    </View>
+                                </View>
+                            </View>
+                        )
+                    }}
+                </Formik>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 20
+    },
+    firstStep: {
+        fontWeight: '700',
+        width: 240,
+        fontSize: 28,
+        lineHeight: 40,
+        marginTop: 80,
+        color: colors.black
+    },
+    button: {
+        height: 62,
+        borderRadius: 12,
+        marginBottom: 20
+    },
+    text: {
+        color: colors.white,
+        textAlign: "center",
+        lineHeight: 62,
+        fontWeight: "500",
+        fontSize: 18
+    }
+});
+
+export default Login;
---
+++ src/page/register/RegisterStep3.tsx
@@ -0,0 +1,140 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Image, Pressable, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ParamListBase, useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useTranslation } from 'react-i18next';
+import colors from '../../constant/color';
+import { HeightDevice, WidthDevice } from '../../util/Dimenssion';
+import { flexRow, flexRowSpaceBetween } from '../../styles/flex';
+import { IMAGE } from '../../constant/image';
+import CategoryDisease from './component/CategoryDisease';
+import { SCREENS_NAME } from '../../navigator/const';
+import ProgressHeader from '../../component/progessHeader';
+
+type dataTypes = {
+    title: string,
+    data: {
+        id: number,
+        name: string
+    }[]
+}[]
+const RegisterStep3 = () => {
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
+    const { t } = useTranslation();
+    const [data, setData] = useState<dataTypes>([
+        {
+            title: t("common.diseases.chronic"),
+            data: [
+                { id: 1, name: t("common.diseases.cholesterol") },
+                { id: 2, name: t("common.diseases.highBlood") },
+                { id: 3, name: t("common.diseases.diabetes") },
+                { id: 4, name: t("common.diseases.osteoporosis") },
+                { id: 5, name: t("common.diseases.kidney") },
+            ]
+        },
+        {
+            title: t("common.diseases.chronicRespiratory"),
+            data: [
+                { id: 6, name: t("common.diseases.congestedLungs") },
+                { id: 7, name: t("common.diseases.asthma") },
+                { id: 8, name: t("common.diseases.pulmonaryFibrosis") },
+                { id: 9, name: t("common.diseases.tuberculosis") },
+                { id: 10, name: t("common.diseases.others") },
+            ]
+        },
+        {
+            title: t("common.diseases.chronicArthritis"),
+            data: [
+                { id: 11, name: t("common.diseases.rheumatoidArthritis") },
+                { id: 12, name: t("common.diseases.gout") },
+                { id: 13, name: t("common.diseases.gout") },
+                { id: 14, name: t("common.diseases.osteoarthritis") },
+            ]
+        }
+    ]);
+    const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+    const loginPage = () => {
+        navigation.navigate(SCREENS_NAME.LOGIN.MAIN);
+    };
+    const handleSelectItem = (itemId: number) => {
+        setSelectedItems((prevSelectedItems) => {
+            if (prevSelectedItems.includes(itemId)) {
+                return prevSelectedItems.filter(item => item !== itemId);
+            } else {
+                return [...prevSelectedItems, itemId];
+            }
+        });
+    };
+    const handleSubmit = () => {
+        if (selectedItems.length > 0) {
+            navigation.navigate(SCREENS_NAME.REGISTER.STEP4);
+        }
+    };
+    return (
+        <ScrollView>
+            <SafeAreaView style={styles.container}>
+                <Pressable onPress={loginPage}>
+                    <Image style={styles.buttonCancel} source={require('../../assets/image/register/icon_X.png')} />
+                </Pressable>
+                <ProgressHeader index={[0, 1, 2]} length={4} style={{ marginTop: 45 }} />
+                <View style={{ marginTop: 20 }}>
+                    <View style={[flexRow, { marginBottom: 30, width: 200, alignItems: 'flex-start' }]}>
+                        <Text style={[styles.hightLight, { color: colors.primary }]}>03.</Text>
+                        <Text style={[styles.hightLight, { color: colors.black }]}>{t("common.text.fillIllness")}</Text>
+                    </View>
+                </View>
+                <View style={{ marginBottom: 70 }}>
+                    {data && data.map((section: any) => {
+                        return (<CategoryDisease key={section.title} section={section} handleSelectItem={handleSelectItem} selectedItems={selectedItems} />)
+                    }
+                    )}
+                </View>
+                <Pressable onPress={handleSubmit} style={[styles.button, { backgroundColor: selectedItems.length !== 0 ? colors.primary : colors.gray }]}>
+                    <Text style={styles.text}>{t("common.text.next")}</Text>
+                </Pressable>
+            </SafeAreaView>
+        </ScrollView>
+    );
+};
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 20,
+        height: HeightDevice,
+    },
+    buttonCancel: {
+        position: 'absolute',
+        right: 0,
+        top: 15,
+    },
+    hightLight: {
+        fontWeight: '700',
+        fontSize: 22,
+        lineHeight: 30,
+    },
+    field: {
+        borderColor: colors.primary,
+        borderWidth: 1,
+        marginTop: 10,
+        borderRadius: 10,
+        paddingHorizontal: 10,
+    },
+    text: {
+        color: colors.white,
+        textAlign: 'center',
+        lineHeight: 62,
+        fontWeight: '500',
+        fontSize: 18,
+    },
+    button: {
+        height: 60,
+        borderRadius: 12,
+        position: 'absolute',
+        bottom: 20,
+        width: WidthDevice - 40,
+        left: 20,
+    },
+
+});
+
+export default RegisterStep3;
