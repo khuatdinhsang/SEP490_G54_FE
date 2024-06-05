904dedfae67a94982e95f5237aca4c5c7d976fae
khuatdinhsang <sangkdhe161711@fpt.edu.vn>
2024-06-06 00:09:29 +0700

    add dimenssion

---
+++ src/util/Dimenssion.ts
@@ -0,0 +1,6 @@
+import {Dimensions} from 'react-native';
+
+const WidthDevice = Dimensions.get('window').width;
+const HeightDevice = Dimensions.get('window').height;
+
+export {WidthDevice, HeightDevice};
