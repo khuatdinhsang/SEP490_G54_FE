66f63d4a3eb50dc6f0724d7ce0e870a40d164f73
khuatdinhsang <sangkdhe161711@fpt.edu.vn>
2024-06-07 00:24:15 +0700

    fix register index

--- src/page/register/index.tsx
+++ src/page/register/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import * as yup from "yup";
-import { Button, Image, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Image, Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { ParamListBase, useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { SCREENS_NAME } from '../../navigator/const';
@@ -13,67 +13,46 @@ import ProgressHeader from '../../component/progessHeader';
 
 const Register = () => {
     const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
-    const { t, i18n } = useTranslation();
+    const { t } = useTranslation();
     const registerSchema = yup.object().shape({
-        name: yup
-            .string()
-            .required(
-                t("placeholder.err.blank")
-            ),
-        email: yup
-            .string()
-            .email(t("placeholder.err.email"))
-            .required(
-                t("placeholder.err.blank")
-            ),
-        password: yup
-            .string()
-            .required(
-                t("placeholder.err.blank")
-            ),
-
-        confirmPassword: yup
-            .string()
-            .required(
-                t("placeholder.err.blank")
-            )
-            .oneOf([yup.ref('password')], t("placeholder.err.notMatch"))
-        ,
-        numberRegHospital: yup
-            .string()
-            .required(
-                t("placeholder.err.blank")
-            ),
+        name: yup.string().required(t("placeholder.err.blank")),
+        email: yup.string().email(t("placeholder.err.email")).required(t("placeholder.err.blank")),
+        password: yup.string().required(t("placeholder.err.blank")),
+        confirmPassword: yup.string().required(t("placeholder.err.blank")).oneOf([yup.ref('password')], t("placeholder.err.notMatch")),
+        numberRegHospital: yup.string().required(t("placeholder.err.blank")),
     });
+
     const handleSubmit = (values: any): void => {
-        navigation.navigate(SCREENS_NAME.REGISTER.STEP2)
-    }
+        navigation.navigate(SCREENS_NAME.REGISTER.STEP2);
+    };
+
     const clearField = (field: string, setFieldValue: (field: string, value: any) => void) => {
         setFieldValue(field, '');
     };
+
     const loginPage = () => {
-        navigation.navigate(SCREENS_NAME.LOGIN.MAIN)
-    }
+        navigation.navigate(SCREENS_NAME.LOGIN.MAIN);
+    };
 
     return (
-        <ScrollView>
-            <SafeAreaView style={styles.container} >
-                <Pressable onPress={loginPage}>
-                    <Image style={styles.buttonCancel} source={require('../../assets/image/register/icon_X.png')} />
-                </Pressable>
-                <ProgressHeader index={[0]} length={4} style={{ marginTop: 45 }} />
-                <Formik
-                    initialValues={{ name: '', email: '', password: '', confirmPassword: '', numberRegHospital: '' }}
-                    validationSchema={registerSchema}
-                    onSubmit={handleSubmit}
-                >
-                    {({ handleChange, handleBlur, handleSubmit, setFieldValue, values, errors, touched }) => (
-                        <View style={{ marginTop: 20 }}>
-                            <View style={{ marginBottom: 20, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                                <Text style={[styles.hightLight, { color: colors.primary }]}>01.</Text>
-                                <Text style={[styles.hightLight, { color: colors.black }]} >{t("common.text.fillInfo")}</Text>
-                            </View>
-                            <View >
+        <SafeAreaView style={styles.container}>
+            <Pressable onPress={loginPage}>
+                <Image style={styles.buttonCancel} source={require('../../assets/image/register/icon_X.png')} />
+            </Pressable>
+            <ProgressHeader index={[0]} length={4} style={{ marginTop: 45 }} />
+            <Formik
+                initialValues={{ name: '', email: '', password: '', confirmPassword: '', numberRegHospital: '' }}
+                validationSchema={registerSchema}
+                onSubmit={handleSubmit}
+            >
+                {({ handleChange, handleBlur, handleSubmit, setFieldValue, values, errors, touched }) => (
+                    <View style={{ flex: 1 }}>
+                        <ScrollView contentContainerStyle={{ paddingBottom: 100 }} showsVerticalScrollIndicator={false}>
+                            <View style={{ marginTop: 20 }}>
+                                <View style={styles.header}>
+                                    <Text style={[styles.hightLight, { color: colors.primary }]}>01.</Text>
+                                    <Text style={[styles.hightLight, { color: colors.black }]}>{t("common.text.fillInfo")}</Text>
+                                </View>
                                 <InputComponent
                                     placeholder={t("placeholder.field.name")}
                                     onPressIconRight={() => clearField('name', setFieldValue)}
@@ -83,91 +62,115 @@ const Register = () => {
                                     label={t("common.text.name")}
                                     textError={errors.name}
                                 />
+                                <View style={{ marginTop: 15 }}>
+                                    <InputComponent
+                                        placeholder={t("placeholder.field.email")}
+                                        onPressIconRight={() => clearField('email', setFieldValue)}
+                                        isIconRight={true}
+                                        value={values.email}
+                                        onChangeText={handleChange('email')}
+                                        label={t("authentication.email")}
+                                        textError={errors.email}
+                                    />
+                                </View>
+                                <View style={{ marginTop: 15 }}>
+                                    <InputComponent
+                                        placeholder={t("placeholder.field.password")}
+                                        onPressIconRight={() => clearField('password', setFieldValue)}
+                                        isIconRight={true}
+                                        value={values.password}
+                                        onChangeText={handleChange('password')}
+                                        label={t("authentication.password")}
+                                        secureTextEntry={true}
+                                        textError={errors.password}
+                                    />
+                                </View>
+                                <View style={{ marginTop: 15 }}>
+                                    <InputComponent
+                                        placeholder={t("placeholder.field.confirmPassword")}
+                                        onPressIconRight={() => clearField('confirmPassword', setFieldValue)}
+                                        isIconRight={true}
+                                        value={values.confirmPassword}
+                                        onChangeText={handleChange('confirmPassword')}
+                                        label={t("common.text.confirmPassword")}
+                                        secureTextEntry={true}
+                                        textError={errors.confirmPassword}
+                                    />
+                                </View>
+                                <View style={{ marginTop: 15 }}>
+                                    <InputComponent
+                                        placeholder={t("placeholder.field.numberRegHospital")}
+                                        onPressIconRight={() => clearField('numberRegHospital', setFieldValue)}
+                                        isIconRight={true}
+                                        value={values.numberRegHospital}
+                                        onChangeText={handleChange('numberRegHospital')}
+                                        label={t("common.text.numberRegHospital")}
+                                        textError={errors.numberRegHospital}
+                                    />
+                                </View>
                             </View>
-                            <View style={{ marginTop: 15 }}>
-                                <InputComponent
-                                    placeholder={t("placeholder.field.email")}
-                                    onPressIconRight={() => clearField('email', setFieldValue)}
-                                    isIconRight={true}
-                                    value={values.email}
-                                    onChangeText={handleChange('email')}
-                                    label={t("authentication.email")}
-                                    textError={errors.email}
-                                />
-                            </View>
-                            <View style={{ marginTop: 15 }}>
-                                <InputComponent
-                                    placeholder={t("placeholder.field.password")}
-                                    onPressIconRight={() => clearField('password', setFieldValue)}
-                                    isIconRight={true}
-                                    value={values.password}
-                                    onChangeText={handleChange('password')}
-                                    label={t("authentication.password")}
-                                    secureTextEntry={true}
-                                    textError={errors.password}
-                                />
-                            </View>
-                            <View style={{ marginTop: 15 }}>
-                                <InputComponent
-                                    placeholder={t("placeholder.field.confirmPassword")}
-                                    onPressIconRight={() => clearField('confirmPassword', setFieldValue)}
-                                    isIconRight={true}
-                                    value={values.confirmPassword}
-                                    onChangeText={handleChange('confirmPassword')}
-                                    label={t("common.text.confirmPassword")}
-                                    secureTextEntry={true}
-                                    textError={errors.confirmPassword}
-                                />
-                            </View>
-                            <View style={{ marginTop: 15 }}>
-                                <InputComponent
-                                    placeholder={t("placeholder.field.numberRegHospital")}
-                                    onPressIconRight={() => clearField('numberRegHospital', setFieldValue)}
-                                    isIconRight={true}
-                                    value={values.numberRegHospital}
-                                    onChangeText={handleChange('numberRegHospital')}
-                                    label={t("common.text.numberRegHospital")}
-                                    textError={errors.numberRegHospital}
-                                />
-                            </View>
-                            <Pressable onPress={() => handleSubmit()} style={[styles.button, { backgroundColor: (errors.name || errors.email || errors.password || errors.confirmPassword || errors.numberRegHospital) ? colors.gray : colors.primary }]} >
+                        </ScrollView>
+                        <View style={styles.buttonContainer}>
+                            <Pressable
+                                onPress={() => handleSubmit()}
+                                style={[
+                                    styles.button,
+                                    {
+                                        backgroundColor: (errors.name || errors.email || errors.password || errors.confirmPassword || errors.numberRegHospital)
+                                            ? colors.gray
+                                            : colors.primary
+                                    }
+                                ]}
+                            >
                                 <Text style={styles.text}>{t("common.text.next")}</Text>
                             </Pressable>
                         </View>
-                    )}
-                </Formik>
-            </SafeAreaView >
-        </ScrollView>
+                    </View>
+                )}
+            </Formik>
+        </SafeAreaView>
     );
 };
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingHorizontal: 20
+        paddingHorizontal: 20,
     },
     buttonCancel: {
         position: 'absolute',
         right: 0,
-        top: 15
+        top: 15,
     },
     hightLight: {
         fontWeight: '700',
         fontSize: 22,
         lineHeight: 30,
     },
+    header: {
+        marginBottom: 20,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
     text: {
         color: colors.white,
         textAlign: "center",
         lineHeight: 62,
         fontWeight: "500",
-        fontSize: 18
+        fontSize: 18,
     },
     button: {
-        height: 62,
+        height: 60,
         borderRadius: 12,
-        marginTop: 15,
-        marginBottom: 15
     },
-})
+    buttonContainer: {
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+        paddingVertical: 20,
+        backgroundColor: colors.white,
+    },
+});
 
 export default Register;
